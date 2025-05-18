918a24697723fa351682f200b9cc620a04f38fe0
Milton Ahmed <devmiltonahmed@gmail.com>
2025-05-19 00:42:45 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let input = document.querySelector('input');
+let button = document.querySelector('button')
+
+button.addEventListener('click', function(){
+
+  let = multiplicationTable = input.value
+
+  for(let i = 1; i <= 10; i++){
+    document.writeln( multiplicationTable + ' X ' + i + ' = ' + multiplicationTable * i +  '<br/>' + '')
+
+  }
+
+
+})
